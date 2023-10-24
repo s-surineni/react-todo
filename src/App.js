9ea0bc6388c61067e102846a4b67d93715e1a583
Sampath Surineni <sampath.surineni@gmail.com>
2023-10-24 15:19:48 +0530

    started todo form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm'
 
 function App() {
   return (
     <div className="todo-app">
           Todo App
+          <TodoForm />
     </div>
   );
 }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,6 @@
+function TodoForm() {
+    return (
+        <div>Hello</div>
+    )
+}
+export default TodoForm
