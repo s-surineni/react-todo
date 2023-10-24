d1a53bc8e3f5ab0f3a0f381949ed44079a3b1902
Sampath Surineni <sampath.surineni@gmail.com>
2023-10-24 15:58:43 +0530

    added handlesubmit function

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,19 @@
+import { useState } from "react"
+
 function TodoForm() {
+    const [input, setInput] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
-        <form>
-            <input type="text" />
-            <button type="submit">Add</button>
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input type="text"
+                placeholder="Add todo"
+                name="text"
+                value={input}
+                className="todo-input" />
+            <button className="todo-button">Add</button>
         </form>
     )
 }
