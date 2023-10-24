1c82438154ac4128fcc527eb99e7595eefbb48c3
Sampath Surineni <sampath.surineni@gmail.com>
2023-10-24 19:53:18 +0530

    added todo list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import TodoForm from "./TodoForm";
+
+function TodoList() {
+    return (
+        <div className="todo-list">
+            <h1>What is the plan</h1>
+            <TodoForm />
+        </div>
+    );
+}
+
+export default TodoList;
