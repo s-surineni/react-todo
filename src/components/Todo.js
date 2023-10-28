20b57bed7964bd011e58d83140c6afdffaa4f736
Sampath Surineni <sampath.surineni@gmail.com>
2023-10-28 16:41:24 +0530

    added icons to todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,16 @@
-function Todo({todos}) {
+import { RiCloseCircleLine } from 'react-icons/ri'
+import { TiEdit } from 'react-icons/ti'
+
+function Todo({ todos }) {
     return todos.map((todo, index) => (
-        <div className="todo" key={index}>
-            {todo.text}
+        <div>
+            <div className="todo" key={index}>
+                {todo.text}
+            </div>
+            <div className="icons">
+                <RiCloseCircleLine />
+                <TiEdit />
+            </div>
         </div>
     ))
 }
