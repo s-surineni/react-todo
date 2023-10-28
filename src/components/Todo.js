8ce4cc20caf6c1885886297ac031e992a6422d44
Sampath Surineni <sampath.surineni@gmail.com>
2023-10-28 16:48:23 +0530

    implemented remove todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,14 @@
 import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
 
-function Todo({ todos }) {
+function Todo({ todos, removeTodo }) {
     return todos.map((todo, index) => (
         <div>
             <div className="todo" key={index}>
                 {todo.text}
             </div>
             <div className="icons">
-                <RiCloseCircleLine />
+                <RiCloseCircleLine onClick={() => removeTodo(todo.id)} />
                 <TiEdit />
             </div>
         </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,6 +5,16 @@ import TodoForm from "./TodoForm";
 function TodoList() {
     const [todos, setTodos] = useState([])
 
+    const completeTodo = (id) => {
+        let updatedTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(updatedTodos)
+    }
+
     const addTodo = (todo) => {
         console.log(todo)
         if (!todo.text || /^\s*$/.test(todo.text)) {
@@ -13,11 +23,16 @@ function TodoList() {
         const newTodos = [todo, ...todos]
         setTodos(newTodos)
     }
+
+    const removeTodo = (id) => {
+        const removeArr = [...todos].filter((todo) => todo.id !== id)
+        setTodos(removeArr)
+    }
     return (
         <div className="todo-list">
             <h1>What is the plan</h1>
             <TodoForm onSubmit={addTodo}/>
-            <Todo todos={todos}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}/>
         </div>
     );
 }
