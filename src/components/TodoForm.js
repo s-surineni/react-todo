20887a6c8b52d5fefd56dc1135e7dbc37d341492
Sampath Surineni <sampath.surineni@gmail.com>
2023-10-29 16:30:54 +0530

    added feature for updating

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,24 +1,40 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
-function TodoForm({onSubmit}) {
-    const [input, setInput] = useState("")
+function TodoForm(props) {
+    const [input, setInput] = useState(props.edit ? props.edit.value : "")
+
     const handleChange = (e) => {
         setInput(e.target.value)
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        onSubmit({id: Math.floor(Math.random() * 1000), text: input})
+        props.onSubmit({ id: Math.floor(Math.random() * 1000), text: input })
         setInput("")
     }
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
-            <input type="text"
-                placeholder="Add todo"
-                name="text"
-                value={input}
-                className="todo-input"
-                onChange={handleChange} />
-            <button className="todo-button">Add</button>
+            {props.edit ? (
+                <>
+                    <input type="text"
+                        placeholder="Update your item"
+                        name="text"
+                        value={input}
+                        className="todo-input edit"
+                        onChange={handleChange}
+                    />
+                    <button className="todo-button edit">Update</button>
+                </>
+            ) : (
+                <>
+                    <input type="text"
+                        placeholder="Add todo"
+                        name="text"
+                        value={input}
+                        className="todo-input"
+                        onChange={handleChange} />
+                    <button className="todo-button">Add</button>
+                </>
+            )}
         </form>
     )
 }
